ddafccd5728e41326b3162da7ece5ec5bd8cfb19
killi <killi@DESKTOP-CBKFDRT>
2019-09-24 11:26:59 +0200

    Architecture Projet

---
+++ MadMax22/src/Controler/wkf_decrypt.java
@@ -0,0 +1,10 @@
+package Controler;
+
+//Gestion des fichiers
+public class wkf_decrypt {
+
+	boolean pcs_decrypter(String source_path, String destination_path) {
+		return false; // à modifier si besoin
+		//Contenu méthode
+	}
+}
