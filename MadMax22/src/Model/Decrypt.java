98bc6d8480d01517f89e2411972e89b684181a28
APZEY <antonin.pretet@orange.fr>
2019-09-24 16:02:09 +0200

    Fonction de lecture de fichier FIles.class et fonction de cryptage Decrypt.class

--- MadMax22/src/Model/Decrypt.java
+++ MadMax22/src/Model/Decrypt.java
@@ -2,4 +2,20 @@ package Model;
 
 public class Decrypt {
 
+	//Pour crypter ou décrypter un message 
+	public String crypter(String texte, String Key) {
+		String chaineCryptee = "";
+		
+		for(int i = 0, j = 0; i < texte.length(); i++)
+		{
+			chaineCryptee += (char)((int)texte.charAt(i) ^ (int)Key.charAt(j));
+			j++;
+			
+			if(j>=Key.length()) j = 0;
+		}
+		return chaineCryptee;
+	}
+	
+	
+	
 }
--- MadMax22/src/Model/Files.java
+++ MadMax22/src/Model/Files.java
@@ -1,11 +1,35 @@
 package Model;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import javax.swing.JFileChooser;
 
 public class Files {
 
 		
+	
+	public String lireFichierSimple(String path) {
+		String txtFile = null;
+		try{
+			 buf = new BufferedReader(new FileReader(path));
+			 txtFile = buf.readLine();
+//			 
+//			FileReader file = new FileReader(path);
+//			txtFile = file.read();
+			 
+		}catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return txtFile;
+	}
+	
+	
 		 public static void OpenFile(String[] arg) throws IOException {
 				JFileChooser dialogue = new JFileChooser(new File("."));
 				File fichier;
