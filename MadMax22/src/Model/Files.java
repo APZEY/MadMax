58349d4093c4b51373b0aba139a998e606b1ae3d
Adrien Paumier <38352466+Dartheagle@users.noreply.github.com>
2019-09-24 15:41:00 +0200

    Method qui ouvre le fichier a decrypter

--- MadMax22/src/Model/Files.java
+++ MadMax22/src/Model/Files.java
@@ -1,5 +1,20 @@
 package Model;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JFileChooser;
 
 public class Files {
+
+		
+		 public static void OpenFile(String[] arg) throws IOException {
+				JFileChooser dialogue = new JFileChooser(new File("."));
+				File fichier;
+				
+				if (dialogue.showOpenDialog(null)== 
+				    JFileChooser.APPROVE_OPTION) {
+				    fichier = dialogue.getSelectedFile();
+				  
+				}
+			}
 
 }
